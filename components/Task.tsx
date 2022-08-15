863f820e58c449d571ffbb639f183e590a29282d
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-08-16 00:31:14 +0200

    s

--- components/Task.tsx
+++ components/Task.tsx
@@ -5,7 +5,7 @@ const Task = ({ task, deleteTask }: any) => {
     <div className="flex items-center text-white">
       <div className=" bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 text-[#b6c7db] flex w-[70%] rounded-[15px] mb-[10px] flex-1">
         <div className="flex items-center justify-between w-full p-[20px] text-xl">
-          {task.taskId} : {task.taskDescription}
+          #{task.taskId} : {task.taskDescription}
         </div>
       </div>
       <BsFillTrashFill className="text-2xl cursor-pointer ml-10" onClick={deleteTask} />
